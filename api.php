952d492a4aa4f25690e785d081e6708c9fa0ce4a
Sinead Maher <sineadm3@gmail.com>
2017-03-06 12:19:07 +0000

    route modal update

--- api.php
+++ api.php
@@ -1,16 +1,8 @@
 <?php
-
-$host = "localhost";
-$user = "root";
-$pass = "";
-
-$databaseName = "kerry_flyer";
-$tableName = "routes";
-
-include 'inc/config.php';
-$con = mysql_connect($host, $user, $pass);
-$dbs = mysql_select_db($databaseName, $con);
-
+    include 'inc/config.php';
+    if(issit($_POST['display'])){
+        $result = mysql_query("SELECT * FROM routes");
+    }
 
 $result = mysql_query("SELECT * FROM $tableName where driver ='TJ Nelligan'");
 $array = mysql_fetch_array($result);
